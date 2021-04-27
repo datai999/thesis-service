257ef71b77b20d994c594599956a20823b89f8e5
Đức Anh Tài <datai28599@gmail.com>
2021-04-28 00:08:29 +0700

    init

---
+++ src/main/java/com/thesis/service/ThesisApplication.java
@@ -0,0 +1,13 @@
+package com.thesis.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThesisApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/thesis/service/ThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesis.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
