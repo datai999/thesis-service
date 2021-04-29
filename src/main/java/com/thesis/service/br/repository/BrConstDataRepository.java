4402a1dd8fdbe89375b137529d917b074b2a19c5
Đức Anh Tài <datai28599@gmail.com>
2021-04-30 03:13:33 +0700

    BrConstDataRepository

---
+++ src/main/java/com/thesis/service/br/repository/BrConstDataRepository.java
@@ -0,0 +1,11 @@
+package com.thesis.service.br.repository;
+
+import com.thesis.service.model.br.BrConstDataTable;
+import com.thesis.service.repository.BaseRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrConstDataRepository extends BaseRepository<BrConstDataTable> {
+
+}
