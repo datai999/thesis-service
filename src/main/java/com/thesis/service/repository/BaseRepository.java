61fc46cf3b2c8e93edb5397948b494e0a9aa4207
Đức Anh Tài <datai28599@gmail.com>
2021-04-30 00:34:40 +0700

    BaseRepository

--- src/main/java/com/thesis/service/repository/BaseRepository.java
+++ src/main/java/com/thesis/service/repository/BaseRepository.java
@@ -1,7 +1,9 @@
 package com.thesis.service.repository;
 
+import com.thesis.service.model.BaseTable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BaseRepository<E> extends JpaRepository<E, Long> {
+public interface BaseRepository<E extends BaseTable> extends JpaRepository<E, Long> {
 
 }
