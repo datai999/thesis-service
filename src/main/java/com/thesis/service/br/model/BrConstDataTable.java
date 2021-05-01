758438b87dad6c282d4d7f2085381297e9fa281f
Đức Anh Tài <datai28599@gmail.com>
2021-05-01 22:59:17 +0700

    unique BrConstDataTable {type,value}

--- src/main/java/com/thesis/service/br/model/BrConstDataTable.java
+++ src/main/java/com/thesis/service/br/model/BrConstDataTable.java
@@ -2,6 +2,7 @@ package com.thesis.service.br.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
 import com.thesis.service.common.model.BaseTable;
@@ -10,7 +11,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Entity
-@Table(name = "br_const_data")
+@Table(name = "br_const_data", uniqueConstraints = { @UniqueConstraint(columnNames = { "type", "value" }) })
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class BrConstDataTable extends BaseTable {
