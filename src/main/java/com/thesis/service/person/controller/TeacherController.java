c16b89cf773ac1ee4db0d3319e274664bb0b07ea
Đức Anh Tài <datai28599@gmail.com>
2021-05-04 23:36:37 +0700

    search teacher ilike name

--- src/main/java/com/thesis/service/person/controller/TeacherController.java
+++ src/main/java/com/thesis/service/person/controller/TeacherController.java
@@ -1,9 +1,11 @@
 package com.thesis.service.person.controller;
 
 import com.thesis.service.common.controller.EntityController;
+import com.thesis.service.common.dto.response.WrapResponse;
 import com.thesis.service.person.model.PsTeacherTable;
 import com.thesis.service.person.repository.PsTeacherRepository;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,11 @@ public class TeacherController extends EntityController<PsTeacherTable, PsTeache
   @Override
   public String declareBaseService() {
     return "teacher";
+  }
+
+  @GetMapping("search")
+  public Object search(String value) {
+    return WrapResponse.data(super.repository.searchIlikeName(value));
   }
 
 }
--- src/main/java/com/thesis/service/person/repository/PsTeacherRepository.java
+++ src/main/java/com/thesis/service/person/repository/PsTeacherRepository.java
@@ -1,11 +1,17 @@
 package com.thesis.service.person.repository;
 
+import java.util.List;
+
 import com.thesis.service.common.repository.BaseRepository;
 import com.thesis.service.person.model.PsTeacherTable;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PsTeacherRepository extends BaseRepository<PsTeacherTable> {
 
+  @Query(value = "SELECT * FROM ps_teacher WHERE name ILIKE %:value%", nativeQuery = true)
+  List<PsTeacherTable> searchIlikeName(@Param("value") String value);
 }
