89125245f11b3c1d2804c9bd70bf32ca75bde094
Đức Anh Tài <datai28599@gmail.com>
2021-05-05 15:44:34 +0700

    search student by name or student code

--- src/main/java/com/thesis/service/person/controller/StudentController.java
+++ src/main/java/com/thesis/service/person/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.thesis.service.person.controller;
 
 import com.thesis.service.common.controller.EntityController;
+import com.thesis.service.common.dto.response.WrapResponse;
 import com.thesis.service.person.model.PsStudentTable;
 import com.thesis.service.person.repository.PsStudentRepository;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,11 @@ public class StudentController extends EntityController<PsStudentTable, PsStuden
   @Override
   public String declareBaseService() {
     return "student";
+  }
+
+  @GetMapping("search")
+  public Object search(String value) {
+    return WrapResponse.data(super.repository.searchIlikeNameOrCode(value));
   }
 
 }
--- src/main/java/com/thesis/service/person/repository/PsStudentRepository.java
+++ src/main/java/com/thesis/service/person/repository/PsStudentRepository.java
@@ -1,11 +1,19 @@
 package com.thesis.service.person.repository;
 
+import java.util.List;
+
 import com.thesis.service.common.repository.BaseRepository;
 import com.thesis.service.person.model.PsStudentTable;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PsStudentRepository extends BaseRepository<PsStudentTable> {
+
+  @Query(value = "SELECT * FROM ps_student "
+      + "WHERE name ILIKE %:value% OR CAST(student_code AS VARCHAR) ILIKE %:value%", nativeQuery = true)
+  List<PsStudentTable> searchIlikeNameOrCode(@Param("value") String value);
 
 }
