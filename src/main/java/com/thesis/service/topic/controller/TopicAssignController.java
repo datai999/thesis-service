169c5b376c304f86abf74ce172abee810fd871ab
Đức Anh Tài <datai28599@gmail.com>
2021-05-08 23:54:04 +0700

    get topic assign

--- src/main/java/com/thesis/service/topic/controller/TopicAssignController.java
+++ src/main/java/com/thesis/service/topic/controller/TopicAssignController.java
@@ -1,9 +1,16 @@
 package com.thesis.service.topic.controller;
 
+import java.util.stream.Collectors;
+
 import com.thesis.service.common.controller.EntityController;
+import com.thesis.service.common.dto.response.WrapResponse;
+import com.thesis.service.person.repository.PsStudentRepository;
+import com.thesis.service.person.repository.PsTeacherRepository;
 import com.thesis.service.topic.model.TpTopicAssignTable;
 import com.thesis.service.topic.repository.TpTopicAssignRepository;
 
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +21,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TopicAssignController extends EntityController<TpTopicAssignTable, TpTopicAssignRepository> {
 
+  private final PsStudentRepository studentRepository;
+  private final PsTeacherRepository teacherRepository;
+
   @Override
   public String declareBaseService() {
     return "topicAssign";
+  }
+
+  @Override
+  @GetMapping
+  public Object findAll() {
+    var result = super.repository.findAll();
+    result.parallelStream().forEach(x -> {
+      if (!CollectionUtils.isEmpty(x.getExecuteStudentId()))
+        x.setExecuteStudent(
+            studentRepository.findAllById(x.getExecuteStudentId()).stream().collect(Collectors.toSet()));
+      if (!CollectionUtils.isEmpty(x.getGuideTeacherId()))
+        x.setGuideTeacher(teacherRepository.findAllById(x.getGuideTeacherId()).stream().collect(Collectors.toSet()));
+    });
+    return WrapResponse.data(result);
   }
 
 }
--- src/main/java/com/thesis/service/topic/model/TpTopicAssignTable.java
+++ src/main/java/com/thesis/service/topic/model/TpTopicAssignTable.java
@@ -1,39 +1,48 @@
 package com.thesis.service.topic.model;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import com.thesis.service.common.model.BaseTable;
+import com.thesis.service.person.model.PsStudentTable;
 import com.thesis.service.person.model.PsTeacherTable;
 
 import org.hibernate.annotations.Type;
+import org.springframework.util.CollectionUtils;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 @Entity
 @Table(name = "tp_topic_assign")
 public class TpTopicAssignTable extends BaseTable {
 
+  @NonNull
   @OneToOne(cascade = { CascadeType.PERSIST })
   @JoinColumn(name = "topic_id", referencedColumnName = "id")
   private TpTopicTable topic;
 
   @Type(type = "long-array")
   @Column(name = "execute_student_id", columnDefinition = "bigint[]")
-  private Set<Long> executeStudentIds;
+  private Set<Long> executeStudentId;
 
   @Type(type = "long-array")
   @Column(name = "guide_teacher_id", columnDefinition = "bigint[]")
-  private Set<Long> guideTeacherIds;
+  private Set<Long> guideTeacherId;
 
   @OneToOne
   @JoinColumn(name = "review_teacher_id")
@@ -42,5 +51,19 @@ public class TpTopicAssignTable extends BaseTable {
   @OneToOne
   @JoinColumn(name = "council_id")
   private TpCouncilTable council;
+
+  @Transient
+  private Set<PsStudentTable> executeStudent;
+
+  @Transient
+  private Set<PsTeacherTable> guideTeacher;
+
+  @PrePersist
+  private void prePersist() {
+    if (!CollectionUtils.isEmpty(this.executeStudent))
+      this.executeStudentId = this.executeStudent.stream().map(BaseTable::getId).collect(Collectors.toSet());
+    if (!CollectionUtils.isEmpty(this.guideTeacher))
+      this.guideTeacherId = this.guideTeacher.stream().map(BaseTable::getId).collect(Collectors.toSet());
+  }
 
 }
