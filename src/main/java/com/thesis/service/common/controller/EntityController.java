b00e75ff4a94e2e882f226dc4dded17174619457
Đức Anh Tài <datai28599@gmail.com>
2021-05-09 21:14:54 +0700

    add @Validated

--- src/main/java/com/thesis/service/common/controller/EntityController.java
+++ src/main/java/com/thesis/service/common/controller/EntityController.java
@@ -19,12 +19,14 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Validated
 public abstract class EntityController<E extends BaseTable, R extends BaseRepository<E>> {
 
   @Autowired
