776fad277f99a24131f7fd944cfb56e11fc5915a
Đức Anh Tài <datai28599@gmail.com>
2021-05-22 01:52:15 +0700

    auto map id

--- src/main/java/com/thesis/service/common/model/BaseTable.java
+++ src/main/java/com/thesis/service/common/model/BaseTable.java
@@ -1,7 +1,10 @@
 package com.thesis.service.common.model;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
 import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
@@ -19,6 +22,7 @@ import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.TextType;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.util.CollectionUtils;
 
 import lombok.Data;
 
@@ -44,5 +48,40 @@ public abstract class BaseTable implements Serializable {
 
   @UpdateTimestamp
   private Instant updatedAt;
+
+  @SuppressWarnings("unchecked")
+  public Object mapId() {
+
+    var fields = List.of(this.getClass().getDeclaredFields());
+
+    var fieldNames = fields.parallelStream().map(Field::getName).collect(Collectors.toList());
+
+    fields.parallelStream().forEach(field -> {
+
+      if (!fieldNames.contains(field.getName().concat("Id")))
+        return;
+
+      try {
+        var toField = this.getClass().getDeclaredField(field.getName().concat("Id"));
+
+        field.setAccessible(true);
+        toField.setAccessible(true);
+
+        var value = field.get(this);
+
+        if (Iterable.class.isAssignableFrom(field.getType())) {
+          List<BaseTable> valueList = List.class.cast(value);
+          if (!CollectionUtils.isEmpty(valueList)) {
+            toField.set(this, valueList.stream().map(BaseTable::getId).collect(Collectors.toList()));
+          }
+        } else {
+          toField.set(this, BaseTable.class.cast(value).getId());
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    });
+    return this;
+  }
 
 }
