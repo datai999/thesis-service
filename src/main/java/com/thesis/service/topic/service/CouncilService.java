a918003753a653040026916ec04b218ce2955906
Đức Anh Tài <datai28599@gmail.com>
2021-06-02 23:32:59 +0700

    build council

--- src/main/java/com/thesis/service/topic/service/CouncilService.java
+++ src/main/java/com/thesis/service/topic/service/CouncilService.java
@@ -1,6 +1,13 @@
 package com.thesis.service.topic.service;
 
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import com.thesis.service.br.ConstDataService;
 import com.thesis.service.common.service.ABaseService;
+import com.thesis.service.person.model.PsTeacherTable;
+import com.thesis.service.person.service.TeacherService;
 import com.thesis.service.topic.model.TpCouncilTable;
 import com.thesis.service.topic.repository.TpCouncilRepository;
 
@@ -11,5 +18,26 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class CouncilService extends ABaseService<TpCouncilTable, TpCouncilRepository> {
+
+  final ConstDataService constDataService;
+  final TeacherService teacherService;
+
+  @Override
+  public TpCouncilTable build(TpCouncilTable entity) {
+    entity.setById(constDataService, "role");
+    entity.setById(teacherService, "teacher");
+
+    if (Objects.isNull(entity.getRoleId())) {
+      return entity;
+    }
+
+    // set arrReacher fit with arrTeacherId
+    var teacherMap = entity.getTeacher().parallelStream()
+        .collect(Collectors.toMap(PsTeacherTable::getId, Function.identity()));
+    entity.getTeacher().clear();
+    entity.getTeacherId().forEach(teacherId -> entity.getTeacher().add(teacherMap.get(teacherId)));
+
+    return entity;
+  }
 
 }
