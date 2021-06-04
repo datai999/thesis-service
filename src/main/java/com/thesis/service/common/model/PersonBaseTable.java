f57719ece7717a21075b6ec096a7c879e595ff9c
Đức Anh Tài <datai28599@gmail.com>
2021-06-05 00:35:02 +0700

    add code for all person

--- src/main/java/com/thesis/service/common/model/PersonBaseTable.java
+++ src/main/java/com/thesis/service/common/model/PersonBaseTable.java
@@ -1,7 +1,9 @@
 package com.thesis.service.common.model;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,6 +12,10 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @MappedSuperclass
 public abstract class PersonBaseTable extends BaseTable {
+
+  @NotNull
+  @Column(unique = true)
+  private int code;
 
   @NotBlank
   private String name;
