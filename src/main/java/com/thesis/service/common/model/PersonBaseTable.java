21ea1e957570cc9dd042aadb62b68116c9dce85d
Đức Anh Tài <datai28599@gmail.com>
2021-06-05 18:52:22 +0700

    gender: string -> constTable

--- src/main/java/com/thesis/service/common/model/PersonBaseTable.java
+++ src/main/java/com/thesis/service/common/model/PersonBaseTable.java
@@ -1,9 +1,13 @@
 package com.thesis.service.common.model;
 
 import javax.persistence.Column;
+import javax.persistence.JoinColumn;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import com.thesis.service.br.model.BrConstDataTable;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -24,6 +28,7 @@ public abstract class PersonBaseTable extends BaseTable {
 
   private String phone;
 
-  private String gender;
-
+  @OneToOne
+  @JoinColumn(name = "gender_id")
+  private BrConstDataTable gender;
 }
