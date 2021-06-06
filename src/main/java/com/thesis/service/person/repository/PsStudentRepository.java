b248cae785213569ad36f33c64fc25d394baaadd
Đức Anh Tài <datai28599@gmail.com>
2021-06-06 11:59:58 +0700

    bug search student code

--- src/main/java/com/thesis/service/person/repository/PsStudentRepository.java
+++ src/main/java/com/thesis/service/person/repository/PsStudentRepository.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Repository;
 public interface PsStudentRepository extends BaseRepository<PsStudentTable> {
 
   @Query(value = "SELECT * FROM ps_student "
-      + "WHERE name ILIKE %:value% OR CAST(student_code AS VARCHAR) ILIKE %:value%", nativeQuery = true)
+      + "WHERE name ILIKE %:value% OR CAST(code AS VARCHAR) ILIKE %:value%", nativeQuery = true)
   List<PsStudentTable> searchIlikeNameOrCode(@Param("value") String value);
 
 }
