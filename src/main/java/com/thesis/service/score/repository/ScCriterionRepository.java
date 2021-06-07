33f2316b4c334e0bda8f432033866c1a2fd1b093
Đức Anh Tài <datai28599@gmail.com>
2021-06-07 17:37:08 +0700

    init criterion repository

---
+++ src/main/java/com/thesis/service/score/repository/ScCriterionRepository.java
@@ -0,0 +1,11 @@
+package com.thesis.service.score.repository;
+
+import com.thesis.service.common.repository.BaseRepository;
+import com.thesis.service.score.model.ScCriterionTable;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScCriterionRepository extends BaseRepository<ScCriterionTable> {
+
+}
---
+++ src/main/java/com/thesis/service/score/repository/ScCriterionTemplateRepository.java
@@ -0,0 +1,11 @@
+package com.thesis.service.score.repository;
+
+import com.thesis.service.common.repository.BaseRepository;
+import com.thesis.service.score.model.ScCriterionTemplateTable;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScCriterionTemplateRepository extends BaseRepository<ScCriterionTemplateTable> {
+
+}
