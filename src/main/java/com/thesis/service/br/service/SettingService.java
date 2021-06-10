6ca92ef495ea1b94a64873ec469b5c68a807b677
Đức Anh Tài <datai28599@gmail.com>
2021-06-10 23:47:34 +0700

    init setting service

---
+++ src/main/java/com/thesis/service/br/service/SettingService.java
@@ -0,0 +1,20 @@
+package com.thesis.service.br.service;
+
+import com.thesis.service.br.model.BrSettingTable;
+import com.thesis.service.br.repository.BrSettingRepository;
+import com.thesis.service.common.service.ABaseService;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SettingService extends ABaseService<BrSettingTable, BrSettingRepository> implements BrSettingRepository {
+
+  @Override
+  protected void preBuild(BrSettingTable entity) {
+    // do nothing
+  }
+
+}
