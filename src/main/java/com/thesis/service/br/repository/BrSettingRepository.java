a3fe2274cefe45495da1d9dbd19bb101fddd1508
Đức Anh Tài <datai28599@gmail.com>
2021-06-10 23:47:25 +0700

    init setting repository

---
+++ src/main/java/com/thesis/service/br/repository/BrSettingRepository.java
@@ -0,0 +1,11 @@
+package com.thesis.service.br.repository;
+
+import com.thesis.service.br.model.BrSettingTable;
+import com.thesis.service.common.repository.BaseRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrSettingRepository extends BaseRepository<BrSettingTable> {
+
+}
