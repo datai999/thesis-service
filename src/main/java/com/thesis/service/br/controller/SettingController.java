c4923e478fbdd4e65c38aeed43b7c420ad5cb80d
Đức Anh Tài <datai28599@gmail.com>
2021-06-11 15:25:00 +0700

    init setting template api

--- src/main/java/com/thesis/service/br/controller/SettingController.java
+++ src/main/java/com/thesis/service/br/controller/SettingController.java
@@ -1,9 +1,14 @@
 package com.thesis.service.br.controller;
 
+import javax.validation.Valid;
+
 import com.thesis.service.br.model.BrSettingTable;
 import com.thesis.service.br.service.SettingService;
 import com.thesis.service.common.controller.ABaseController;
+import com.thesis.service.score.model.ScCriterionTemplateTable;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +19,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SettingController extends ABaseController<BrSettingTable, SettingService> {
 
+  @PostMapping("/topic-template")
+  public <D extends ScCriterionTemplateTable> Object settingTopicTemplate(@RequestBody @Valid D requestBody) {
+    return null;
+  }
+
+  @PostMapping("/thesis-template")
+  public <D extends ScCriterionTemplateTable> Object settingThesisTemplate(@RequestBody @Valid D requestBody) {
+    return null;
+  }
 }
