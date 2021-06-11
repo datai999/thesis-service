229ad0a2e7d1621b1c44a941bdb7bc0ee035615f
Đức Anh Tài <datai28599@gmail.com>
2021-06-11 20:08:14 +0700

    setting service

--- src/main/java/com/thesis/service/br/controller/SettingController.java
+++ src/main/java/com/thesis/service/br/controller/SettingController.java
@@ -2,13 +2,11 @@ package com.thesis.service.br.controller;
 
 import javax.validation.Valid;
 
-import com.thesis.service.br.model.BrConstDataTable;
 import com.thesis.service.br.model.BrSettingTable;
 import com.thesis.service.br.service.SettingService;
 import com.thesis.service.common.controller.ABaseController;
 import com.thesis.service.score.model.ScCriterionTemplateTable;
 
-import org.springframework.data.domain.Example;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +20,8 @@ public class SettingController extends ABaseController<BrSettingTable, SettingSe
   @PostMapping("/template")
   public <D extends ScCriterionTemplateTable> Object settingTopicTemplate(
       @RequestParam(defaultValue = "true") boolean thesis, @RequestBody @Valid D requestBody) {
-
-    var type = thesis ? "thesis" : "topic";
-    var constDataExample = BrConstDataTable.type("setting.".concat(type).concat("Template"));
-    var constDataRecord = super.constService.findAll(Example.of(constDataExample)).stream().findFirst().orElseThrow();
-
-    var entity = new BrSettingTable(constDataRecord, ScCriterionTemplateTable.TABLE, requestBody);
-    return super.service.save(entity);
+    var type = thesis ? "setting.thesisTemplate" : "setting.topicTemplate";
+    return super.service.setting(type, requestBody);
   }
 
 }
--- src/main/java/com/thesis/service/br/service/SettingService.java
+++ src/main/java/com/thesis/service/br/service/SettingService.java
@@ -1,9 +1,12 @@
 package com.thesis.service.br.service;
 
+import com.thesis.service.br.model.BrConstDataTable;
 import com.thesis.service.br.model.BrSettingTable;
 import com.thesis.service.br.repository.BrSettingRepository;
+import com.thesis.service.common.model.BaseTable;
 import com.thesis.service.common.service.ABaseService;
 
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +18,13 @@ public class SettingService extends ABaseService<BrSettingTable, BrSettingReposi
   @Override
   protected void preBuild(BrSettingTable entity) {
     // do nothing
+  }
+
+  public <T extends BaseTable> Object setting(String type, T refRecord) {
+    var constDataExample = Example.of(BrConstDataTable.type(type));
+    var constDataRecord = super.constRepository.findAll(constDataExample).stream().findFirst().orElseThrow();
+    var entity = new BrSettingTable(constDataRecord, refRecord);
+    return super.save(entity);
   }
 
 }
