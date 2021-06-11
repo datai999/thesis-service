fcffcf7803c03f0983793e7ffe0043b573c7895e
Đức Anh Tài <datai28599@gmail.com>
2021-06-11 20:13:33 +0700

    unit name_id of setting table

--- src/main/java/com/thesis/service/br/model/BrSettingTable.java
+++ src/main/java/com/thesis/service/br/model/BrSettingTable.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.thesis.service.common.model.BaseTable;
 
@@ -20,7 +21,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @NoArgsConstructor
-@Table(name = "br_setting")
+@Table(name = "br_setting", uniqueConstraints = { @UniqueConstraint(columnNames = { "name_id" }) })
 @EqualsAndHashCode(callSuper = true)
 public class BrSettingTable extends BaseTable {
 
