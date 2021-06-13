503d157e070a0fb3984d3a73807851d176b46ca9
Đức Anh Tài <datai28599@gmail.com>
2021-06-13 22:50:44 +0700

    set header in interceptor

--- src/main/java/com/thesis/service/common/config/Interceptor.java
+++ src/main/java/com/thesis/service/common/config/Interceptor.java
@@ -3,12 +3,19 @@ package com.thesis.service.common.config;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.thesis.service.common.utils.ContextHolder;
+
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class Interceptor implements HandlerInterceptor {
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    ContextHolder.setRequest(request);
+    log.info("[{}][{}][{}]", ContextHolder.getLang(), request.getMethod(), request.getRequestURL());
     return true;
   }
 
