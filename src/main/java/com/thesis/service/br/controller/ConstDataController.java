0de12a823172d0953b69ccd41555c697f1c07ac0
Đức Anh Tài <datai28599@gmail.com>
2021-06-15 15:11:52 +0700

    add const no

--- src/main/java/com/thesis/service/br/controller/ConstDataController.java
+++ src/main/java/com/thesis/service/br/controller/ConstDataController.java
@@ -9,6 +9,7 @@ import com.thesis.service.br.service.ConstDataService;
 import com.thesis.service.common.controller.ABaseController;
 
 import org.springframework.data.domain.Example;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,8 +34,14 @@ public class ConstDataController extends ABaseController<BrConstDataTable, Const
   @PostMapping("/all")
   public <D extends BrConstDataTable> Object saveAll(@RequestBody List<D> requestBody) {
     return Set.copyOf(requestBody).parallelStream().map(x -> {
-      var exists = super.service.findAll(Example.of(x));
-      return exists.isEmpty() ? super.service.save(x) : exists.get(0);
+      var entityExample = new BrConstDataTable();
+      entityExample.setType(x.getType());
+      entityExample.setNo(x.getNo());
+      var exists = super.service.findAll(Example.of(entityExample));
+      if (!CollectionUtils.isEmpty(exists)) {
+        x.setId(exists.get(0).getId());
+      }
+      return super.service.save(x);
     }).collect(Collectors.toSet());
   }
 
