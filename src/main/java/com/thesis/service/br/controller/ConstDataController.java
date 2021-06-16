f3ffa7351cbe7b4152f7e960b37c2595eb004806
Đức Anh Tài <datai28599@gmail.com>
2021-06-17 00:09:59 +0700

    bug update dupplicate const

--- src/main/java/com/thesis/service/br/controller/ConstDataController.java
+++ src/main/java/com/thesis/service/br/controller/ConstDataController.java
@@ -9,7 +9,6 @@ import com.thesis.service.br.service.ConstDataService;
 import com.thesis.service.common.controller.ABaseController;
 
 import org.springframework.data.domain.Example;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,13 +33,6 @@ public class ConstDataController extends ABaseController<BrConstDataTable, Const
   @PostMapping("/all")
   public <D extends BrConstDataTable> Object saveAll(@RequestBody List<D> requestBody) {
     return Set.copyOf(requestBody).parallelStream().map(x -> {
-      var entityExample = new BrConstDataTable();
-      entityExample.setType(x.getType());
-      entityExample.setNo(x.getNo());
-      var exists = super.service.findAll(Example.of(entityExample));
-      if (!CollectionUtils.isEmpty(exists)) {
-        x.setId(exists.get(0).getId());
-      }
       return super.service.save(x);
     }).collect(Collectors.toSet());
   }
