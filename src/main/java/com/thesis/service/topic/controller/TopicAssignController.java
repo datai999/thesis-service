c202bce563beb98084365be522fc770d53e67138
Đức Anh Tài <datai28599@gmail.com>
2021-07-05 17:44:55 +0700

    format code TopicAssignController

--- src/main/java/com/thesis/service/topic/controller/TopicAssignController.java
+++ src/main/java/com/thesis/service/topic/controller/TopicAssignController.java
@@ -1,23 +1,19 @@
 package com.thesis.service.topic.controller;
 
 import java.util.List;
-
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-
 import com.thesis.service.common.controller.ABaseController;
 import com.thesis.service.common.dto.request.SearchRequest;
 import com.thesis.service.topic.model.TpTopicAssignTable;
 import com.thesis.service.topic.service.TopicAssignExtendService;
 import com.thesis.service.topic.service.TopicAssignService;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -29,12 +25,14 @@ public class TopicAssignController extends ABaseController<TpTopicAssignTable, T
 
   @GetMapping("/search/teacher")
   public List<TpTopicAssignTable> findByTeacherCode(@RequestParam @NotBlank String code,
-      @RequestParam(defaultValue = "semester") String sort, @RequestParam(defaultValue = "true") boolean descend) {
+      @RequestParam(defaultValue = "semester") String sort,
+      @RequestParam(defaultValue = "true") boolean descend) {
     return super.service.findByTeacherCode(code, sort, descend);
   }
 
   @GetMapping("/search/topic")
-  public Object search(@RequestParam String value, @RequestParam(defaultValue = "semester") String sort,
+  public Object search(@RequestParam String value,
+      @RequestParam(defaultValue = "semester") String sort,
       @RequestParam(defaultValue = "true") boolean descend) {
     return super.service.searchIlikeTopicName(value, sort, descend);
   }
