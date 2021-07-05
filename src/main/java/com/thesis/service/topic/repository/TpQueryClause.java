38ec9478446aa51d5a83854eaafa50c526015bb0
Đức Anh Tài <datai28599@gmail.com>
2021-07-05 11:08:11 +0700

    tTA -> tPA

--- src/main/java/com/thesis/service/topic/repository/TpQueryClause.java
+++ src/main/java/com/thesis/service/topic/repository/TpQueryClause.java
@@ -2,9 +2,11 @@ package com.thesis.service.topic.repository;
 
 public interface TpQueryClause {
 
-  String TOPIC_ASSIGN_INNER_JOIN_TOPIC = "SELECT tTA.* FROM tp_topic_assign tTA INNER JOIN tp_topic tP ON tTA.topic_id = tP.id ";
+  String TOPIC_ASSIGN_INNER_JOIN_TOPIC =
+      "SELECT tPA.* FROM tp_topic_assign tPA INNER JOIN tp_topic tP ON tPA.topic_id = tP.id ";
 
-  String ORDER_TOPIC_SEMESTER = " CASE WHEN :sort = 'semester' AND :isDescend THEN tP.semester END DESC"
-      + " ,CASE WHEN :sort = 'semester' AND NOT :isDescend THEN tP.semester END ASC ";
+  String ORDER_TOPIC_SEMESTER =
+      " CASE WHEN :sort = 'semester' AND :isDescend THEN tP.semester END DESC"
+          + " ,CASE WHEN :sort = 'semester' AND NOT :isDescend THEN tP.semester END ASC ";
 
 }
