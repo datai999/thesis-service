6ed9d4c6fd3fe75d0044df5ead672a81639253dd
Đức Anh Tài <datai28599@gmail.com>
2021-07-05 17:18:10 +0700

    move search function to common service

--- src/main/java/com/thesis/service/common/service/PageService.java
+++ src/main/java/com/thesis/service/common/service/PageService.java
@@ -1,8 +1,12 @@
 package com.thesis.service.common.service;
 
+import java.util.Map;
 import java.util.Objects;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import com.thesis.service.common.dto.DataBaseFieldConst;
 import com.thesis.service.common.dto.request.SearchRequest;
+import com.thesis.service.common.utils.ContextHolder;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +15,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PageService {
+
+  @PersistenceContext
+  private EntityManager entityManager;
 
   public Pageable getPageable(SearchRequest searchRequest) {
 
@@ -37,5 +44,57 @@ public class PageService {
 
     return pageable;
   }
+
+  public int getTotalRecord(String selectClause, String whereClause, String groupClause) {
+    StringBuilder queryCount =
+        new StringBuilder("SELECT COUNT(*) FROM ( ")
+            .append(selectClause)
+            .append(whereClause)
+            .append(groupClause)
+            .append(") main_query");
+
+    return Integer.valueOf(
+        entityManager.createNativeQuery(queryCount.toString()).getSingleResult().toString());
+  }
+
+  public String getSearchWhereQuery(Map<String, Object> fieldFilter) {
+
+    if (fieldFilter.isEmpty())
+      return "";
+
+    var filterQuery = new StringBuilder();
+    fieldFilter.keySet().stream().forEach(filterField -> {
+      String filterEntityField =
+          String.format(DataBaseFieldConst.ENTITY.get(filterField), ContextHolder.getLang());
+      filterQuery.append("AND ").append(filterEntityField).append("::::TEXT ILIKE '%")
+          .append(fieldFilter.get(filterField))
+          .append("%' ");
+    });
+
+    return String.format("WHERE %s", filterQuery.substring(4));
+  }
+
+  public String getSearchOrderQuery(SearchRequest.SortRequest sortRequest) {
+
+    if (Objects.isNull(sortRequest.getField()))
+      return "";
+
+    String sortEntityField = String.format(DataBaseFieldConst.ENTITY.get(sortRequest.getField()),
+        ContextHolder.getLang());
+
+    var orderClause = new StringBuilder("ORDER BY ");
+
+    // FIXME order by sum record field
+    // orderClause.append(String.format(
+    // "CARDINALITY(ARRAY_REMOVE(ARRAY_AGG(%s), NULL)) %s",
+    // sortEntityField, sortRequest.getDescend() ? "DESC" : "ASC"));
+
+    orderClause.append(String.format(
+        " (ARRAY_AGG(%s))[1] %s",
+        sortEntityField, !sortRequest.getDescend() ? "DESC" : "ASC"));
+
+    return orderClause.toString();
+  }
+
 
 }
--- src/main/java/com/thesis/service/topic/service/TopicAssignExtendService.java
+++ src/main/java/com/thesis/service/topic/service/TopicAssignExtendService.java
@@ -1,16 +1,12 @@
 package com.thesis.service.topic.service;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import com.thesis.service.common.dto.DataBaseFieldConst;
 import com.thesis.service.common.dto.request.SearchRequest;
 import com.thesis.service.common.service.PageService;
-import com.thesis.service.common.utils.ContextHolder;
 import com.thesis.service.topic.model.TpTopicAssignTable;
 import com.thesis.service.topic.repository.TpQueryClause;
 import org.springframework.data.domain.Page;
@@ -30,29 +26,25 @@ public class TopicAssignExtendService {
 
   public Page<TpTopicAssignTable> search(SearchRequest requestBody) {
 
-    Pageable pageable = pageService.getPageable(requestBody);
-
-    StringBuilder selectClause = new StringBuilder(TpQueryClause.TOPIC_ASSIGN_SEARCH);
-    String whereClause = this.getWhereQuery(requestBody.getFilter());
+    String selectClause = TpQueryClause.TOPIC_ASSIGN_SEARCH;
+    String whereClause = pageService.getSearchWhereQuery(requestBody.getFilter());
     String groupClause = "GROUP BY tPA.id ";
-    String orderClause = this.getOrderQuery(requestBody.getSort());
+    String orderClause = pageService.getSearchOrderQuery(requestBody.getSort());
 
-    StringBuilder queryCount =
-        new StringBuilder("SELECT COUNT(*) FROM ( ")
-            .append(selectClause)
-            .append(whereClause)
-            .append(groupClause)
-            .append(") main_query");
+    int totalRecord = pageService.getTotalRecord(selectClause, whereClause, groupClause);
 
-    Integer totalRecord = Integer.valueOf(
-        entityManager.createNativeQuery(queryCount.toString()).getSingleResult().toString());
+    Pageable pageable = pageService.getPageable(requestBody);
 
     if (totalRecord == 0)
       return new PageImpl<>(List.of(), pageable, totalRecord);
 
-    Query query = entityManager.createNativeQuery(
-        selectClause.append(whereClause).append(groupClause).append(orderClause).toString(),
-        TpTopicAssignTable.class);
+    String queryClause =
+        new StringBuilder(selectClause)
+            .append(whereClause)
+            .append(groupClause)
+            .append(orderClause)
+            .toString();
+    Query query = entityManager.createNativeQuery(queryClause, TpTopicAssignTable.class);
     query.setFirstResult(pageable.getPageNumber() * pageable.getPageSize());
     query.setMaxResults(pageable.getPageSize());
 
@@ -65,41 +57,6 @@ public class TopicAssignExtendService {
     return new PageImpl<>(queryResponse, pageable, totalRecord);
   }
 
-  private String getWhereQuery(Map<String, Object> fieldFilter) {
 
-    if (fieldFilter.isEmpty())
-      return "";
-
-    var filterQuery = new StringBuilder();
-    fieldFilter.keySet().stream().forEach(filterField -> {
-      String filterEntityField =
-          String.format(DataBaseFieldConst.ENTITY.get(filterField), ContextHolder.getLang());
-      filterQuery.append("AND ").append(filterEntityField).append("::::TEXT ILIKE '%")
-          .append(fieldFilter.get(filterField))
-          .append("%' ");
-    });
-
-    return String.format("WHERE %s", filterQuery.substring(4));
-  }
-
-  private String getOrderQuery(SearchRequest.SortRequest sortRequest) {
-    if (Objects.isNull(sortRequest.getField()))
-      return "";
-    String sortEntityField = String.format(DataBaseFieldConst.ENTITY.get(sortRequest.getField()),
-        ContextHolder.getLang());
-
-    var orderClause = new StringBuilder("ORDER BY ");
-
-    // FIXME order by sum record field
-    // orderClause.append(String.format(
-    // "CARDINALITY(ARRAY_REMOVE(ARRAY_AGG(%s), NULL)) %s",
-    // sortEntityField, sortRequest.getDescend() ? "DESC" : "ASC"));
-
-    orderClause.append(String.format(
-        " (ARRAY_AGG(%s))[1] %s",
-        sortEntityField, !sortRequest.getDescend() ? "DESC" : "ASC"));
-
-    return orderClause.toString();
-  }
 
 }
