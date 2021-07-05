3658ac8cac6f9f3c647fa63be6a5dca34da86e29
Đức Anh Tài <datai28599@gmail.com>
2021-07-05 11:28:30 +0700

    topic const field

--- src/main/java/com/thesis/service/common/dto/DataBaseFieldConst.java
+++ src/main/java/com/thesis/service/common/dto/DataBaseFieldConst.java
@@ -1,17 +1,20 @@
 package com.thesis.service.common.dto;
 
 import java.util.Map;
-
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DataBaseFieldConst {
 
-  public static final Map<String, String> MODAL = Map.ofEntries(Map.entry("guideTeacher", "guideTeacherCode"),
+  public static final Map<String, String> MODAL = Map.ofEntries(
+      Map.entry("guideTeacher", "guideTeacherCode"),
       Map.entry("executeStudent", "executeStudentCode"), Map.entry("topic.major", "topic.majorId"));
 
-  public static final Map<String, String> ENTITY = Map.ofEntries(Map.entry("topic.name", "tP.name::::json->>'%s'"),
-      Map.entry("topic.major", "topic.majorId"));
+  public static final Map<String, String> ENTITY = Map.ofEntries(
+      Map.entry("topic.code", "tP.code"),
+      Map.entry("topic.semester", "tP.semester"),
+      Map.entry("topic.major", "tP.major"),
+      Map.entry("topic.name", "tP.name::::json->>'%s'"));
 
 }
