27bdbf088976ecadbb93173dbd23cec115cde165
Đức Anh Tài <datai28599@gmail.com>
2021-07-05 11:07:09 +0700

    bug null page service

--- src/main/java/com/thesis/service/common/service/PageService.java
+++ src/main/java/com/thesis/service/common/service/PageService.java
@@ -1,10 +1,9 @@
 package com.thesis.service.common.service;
 
 import java.util.Objects;
-
 import com.thesis.service.common.dto.DataBaseFieldConst;
 import com.thesis.service.common.dto.request.SearchRequest;
-
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -14,18 +13,26 @@ import org.springframework.stereotype.Service;
 public class PageService {
 
   public Pageable getPageable(SearchRequest searchRequest) {
+
     // map sort
-    String sortEntityField = DataBaseFieldConst.ENTITY.get(searchRequest.getSort().getField());
-    if (Objects.isNull(sortEntityField)) {
-      sortEntityField = searchRequest.getSort().getField();
+    String sortEntityField = searchRequest.getSort().getField();
+    if (Objects.nonNull(sortEntityField)) {
+      sortEntityField = ObjectUtils.defaultIfNull(
+          DataBaseFieldConst.ENTITY.get(sortEntityField),
+          sortEntityField);
     }
 
-    Pageable pageable = PageRequest.of(searchRequest.getPage().getNumber(), searchRequest.getPage().getSize());
+    Pageable pageable = PageRequest.of(
+        searchRequest.getPage().getNumber(),
+        searchRequest.getPage().getSize());
 
     if (Objects.nonNull(sortEntityField)) {
       Sort sortable = Sort.by(sortEntityField);
-      sortable = searchRequest.getSort().getDescend() ? sortable.descending() : sortable.ascending();
-      pageable = PageRequest.of(searchRequest.getPage().getNumber(), searchRequest.getPage().getSize(), sortable);
+      sortable =
+          searchRequest.getSort().getDescend() ? sortable.descending() : sortable.ascending();
+      pageable = PageRequest.of(
+          searchRequest.getPage().getNumber(),
+          searchRequest.getPage().getSize(), sortable);
     }
 
     return pageable;
