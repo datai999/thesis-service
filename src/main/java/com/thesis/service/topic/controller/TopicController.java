b7ff1f381943d750017c912d893d767134c0ec65
Đức Anh Tài <datai28599@gmail.com>
2021-09-03 18:49:55 +0700

    update api topic

--- src/main/java/com/thesis/service/topic/controller/TopicController.java
+++ src/main/java/com/thesis/service/topic/controller/TopicController.java
@@ -6,21 +6,24 @@ import com.thesis.service.topic.dto.response.TopicFlatResponse;
 import com.thesis.service.topic.model.TpTopicTable;
 import com.thesis.service.topic.repository.TpTopicRepository;
 import com.thesis.service.topic.service.TopicService;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/topic")
+@RequestMapping("/topics")
 public class TopicController
     extends AbstractBaseController<TpTopicTable, TpTopicRepository, TopicService> {
 
-  @GetMapping("/flat")
-  public Object reduce() {
+  @GetMapping("/{type}-flat")
+  public Object reduce(@PathVariable String type) {
+    var example = Example.of(new TpTopicTable().setThesis("thesis".equals(type)));
     Sort sortable = Sort.by(Direction.DESC, "id");
-    return service.getRepository().findAll(sortable).stream()
+    return service.getRepository().findAll(example, sortable).stream()
         .map(TopicFlatResponse::from).collect(Collectors.toList());
   }
 
