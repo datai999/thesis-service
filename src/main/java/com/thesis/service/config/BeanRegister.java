a7c37e9ec75d3be1467069d9a2365c0192fc3ad6
Đức Anh Tài <datai28599@gmail.com>
2021-09-12 11:05:56 +0700

    logging request

--- src/main/java/com/thesis/service/config/BeanRegister.java
+++ src/main/java/com/thesis/service/config/BeanRegister.java
@@ -3,13 +3,26 @@ package com.thesis.service.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 public class BeanRegister {
 
   @Bean
   public ModelMapper modelMapper() {
-    return new ModelMapper();
+    var modelMapper = new ModelMapper();
+    modelMapper.getConfiguration().setSkipNullEnabled(true);
+    return modelMapper;
+  }
+
+  // @Bean
+  public CommonsRequestLoggingFilter logFilter() {
+    var filter = new CommonsRequestLoggingFilter();
+    filter.setIncludeQueryString(true);
+    filter.setIncludePayload(true);
+    filter.setMaxPayloadLength(10000);
+    filter.setIncludeClientInfo(true);
+    return filter;
   }
 
 }
