f9dc419195ccd3352537da6f6d6d58a57c8dd8d0
Đức Anh Tài <datai28599@gmail.com>
2021-09-18 01:00:07 +0700

    init message source

--- src/main/java/com/thesis/service/config/BeanRegister.java
+++ src/main/java/com/thesis/service/config/BeanRegister.java
@@ -1,6 +1,9 @@
 package com.thesis.service.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
@@ -14,6 +17,14 @@ public class BeanRegister {
     filter.setMaxPayloadLength(10000);
     filter.setIncludeClientInfo(true);
     return filter;
+  }
+
+  @Bean
+  public MessageSource messageSource() {
+    var messageSource = new ReloadableResourceBundleMessageSource();
+    messageSource.setBasename("classpath:/messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
   }
 
 }
