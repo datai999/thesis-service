0ba2fbfb971537711de62823cee683b56b1f7eb7
Đức Anh Tài <datai28599@gmail.com>
2021-10-11 16:59:01 +0700

    return setting template id

--- src/main/java/com/thesis/service/dto/score/response/SettingTemplateGroupRoleResponse.java
+++ src/main/java/com/thesis/service/dto/score/response/SettingTemplateGroupRoleResponse.java
@@ -1,15 +1,15 @@
 package com.thesis.service.dto.score.response;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import com.thesis.service.constant.MessageCode;
 import com.thesis.service.constant.TopicRole;
 import com.thesis.service.dto.system.BaseResponse;
-import com.thesis.service.model.score.TemplateTable;
+import com.thesis.service.model.score.SettingTemplateTable;
 import com.thesis.service.model.topic.CouncilRoleTable;
 import com.thesis.service.utils.ContextAccessor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @AllArgsConstructor
@@ -19,20 +19,33 @@ public class SettingTemplateGroupRoleResponse {
   private List<Role> thesis = List.of();
 
   @Data
+  @EqualsAndHashCode(callSuper = false)
+  public static class SettingTemplateResponse extends BaseResponse {
+    private long templateId;
+
+    public SettingTemplateResponse(SettingTemplateTable entity) {
+      ContextAccessor.getModelConverter().map(entity.getTemplate(), this);
+      super.setId(entity.getId());
+      super.setDeleted(entity.isDeleted());
+      this.templateId = entity.getTemplate().getId();
+    }
+  }
+
+  @Data
   public static class Role {
     private String name;
     private boolean thesis;
     private TopicRole topicRole;
     private BaseResponse councilRole;
-    private List<BaseResponse> templates = List.of();
+    private List<SettingTemplateResponse> templates = List.of();
 
     public Role(String name, boolean thesis, TopicRole topicRole,
-        List<TemplateTable> templates) {
+        List<SettingTemplateTable> settingTemplates) {
       this.name = name;
       this.thesis = thesis;
       this.topicRole = topicRole;
       this.templates = ContextAccessor.getModelConverter()
-          .map(templates, BaseResponse.class);
+          .map(settingTemplates, SettingTemplateResponse::new);
     }
 
     public Role(CouncilRoleTable councilRole) {
@@ -40,10 +53,8 @@ public class SettingTemplateGroupRoleResponse {
           ContextAccessor.getMessageSource().getMessage(MessageCode.Council.COUNCIL));
       this.thesis = true;
       this.councilRole = ContextAccessor.getModelConverter().map(councilRole, BaseResponse.class);
-      this.templates = councilRole.getSettingTemplates().parallelStream()
-          .map(criterionRole -> ContextAccessor.getModelConverter()
-              .map(criterionRole.getTemplate(), BaseResponse.class))
-          .collect(Collectors.toList());
+      this.templates = ContextAccessor.getModelConverter()
+          .map(councilRole.getSettingTemplates(), SettingTemplateResponse::new);
     }
   }
 
--- src/main/java/com/thesis/service/repository/score/SettingTemplateRepository.java
+++ src/main/java/com/thesis/service/repository/score/SettingTemplateRepository.java
@@ -3,7 +3,6 @@ package com.thesis.service.repository.score;
 import java.util.List;
 import com.thesis.service.constant.TopicRole;
 import com.thesis.service.model.score.SettingTemplateTable;
-import com.thesis.service.model.score.TemplateTable;
 import com.thesis.service.repository.BaseRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,8 +10,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SettingTemplateRepository extends BaseRepository<SettingTemplateTable> {
 
-  @Query("SELECT scST.template FROM SettingTemplateTable scST " +
+  @Query("SELECT scST FROM SettingTemplateTable scST " +
       "WHERE scST.deleted = FALSE AND scST.thesis = ?1 AND scST.topicRole = ?2")
-  List<TemplateTable> findTemplateByTopicRole(boolean thesis, TopicRole topicRole);
+  List<SettingTemplateTable> findTemplateByTopicRole(boolean thesis, TopicRole topicRole);
 
 }
